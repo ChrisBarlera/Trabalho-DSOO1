bd5e66b752e8d3a475eb9b53ee0aa91e1e659c6c
Christian Barlera <christian.barlera2@gmail.com>
2024-11-25 20:07:05 -0300

    iniciando

--- MVC/limite/TelaGato.py
+++ MVC/limite/TelaGato.py
@@ -1,3 +1,21 @@
+# import PySimpleGUI as sg 
+ 
+# # Very basic window.  Return values as a dictionary 
+ 
+# layout = [ 
+#             [sg.Text('Please enter your Name, Address, Phone')], 
+#             [sg.Text('Name', size=(15, 1)), sg.InputText('name', key='name')],
+#             [sg.Text('Address', size=(15, 1)), sg.InputText('address', key='address')], 
+#             [sg.Text('Phone', size=(15, 1)), sg.InputText('phone', key='phone')], 
+#             [sg.Submit(), sg.Cancel()] 
+#          ] 
+ 
+# window = sg.Window('Simple data entry GUI').Layout(layout) 
+ 
+# button, values = window.Read() 
+ 
+# print(values['name'], values['address'], values['phone'])
+
 class TelaGato:
     def tela_opcoes(self):
         print('\n-------- GATO ----------')
--- MVC/limite/TelaSistema.py
+++ MVC/limite/TelaSistema.py
@@ -1,13 +1,38 @@
+import PySimpleGUI as sg
+
+
 class TelaSistema:
-    #fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
+
+    def __init__(self) -> None:
+        self.__window = None
+
     def tela_opcoes(self):
-        print('\n-------- Sistema para ONG de adoção de animais ---------')
-        print('1 - Animais')
-        print('2 - Pessoas')
-        print('3 - Adoções')
-        print('4 - Doações')
-        print('5 - Habitações')
-        print('6 - Vacinações')
-        print('0 - Finalizar sistema')
-        opcao = int(input('Escolha a opção: '))
+        self.init_components()
+        button, opcao = self.open()
+        # print('\n-------- Sistema para ONG de adoção de animais ---------')
+        # print('1 - Animais')
+        # print('2 - Pessoas')
+        # print('3 - Adoções')
+        # print('4 - Doações')
+        # print('5 - Habitações')
+        # print('6 - Vacinações')
+        # print('0 - Finalizar sistema')
+        # opcao = int(input('Escolha a opção: '))
         return opcao
+
+    def init_components(self):
+        sg.ChangeLookAndFeel('DarkGreen2')
+        layout = [
+            [sg.Text('Um texto')]
+        ]
+        self.__window = sg.Window('Sistema da ONG', default_element_size=(40,1)).Layout(layout)
+
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
+    
+    def close(self):
+        self.__window.Close()
+
+    def show_message(self, titulo: str, mensagem: str):
+        sg.Popup(titulo, mensagem)
