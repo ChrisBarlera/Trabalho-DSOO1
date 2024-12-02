25922c44a431627449c685496e8602c6da7138dd
Christian Barlera <christian.barlera2@gmail.com>
2024-12-02 15:12:13 -0300

    Fazendo primeira tela do sistema

--- MVC/limite/TelaSistema.py
+++ MVC/limite/TelaSistema.py
@@ -5,27 +5,40 @@ class TelaSistema:
 
     def __init__(self) -> None:
         self.__window = None
+        sg.ChangeLookAndFeel('DarkGreen2')
 
     def tela_opcoes(self):
         self.init_components()
-        button, opcao = self.open()
-        # print('\n-------- Sistema para ONG de adoção de animais ---------')
-        # print('1 - Animais')
-        # print('2 - Pessoas')
-        # print('3 - Adoções')
-        # print('4 - Doações')
-        # print('5 - Habitações')
-        # print('6 - Vacinações')
-        # print('0 - Finalizar sistema')
-        # opcao = int(input('Escolha a opção: '))
-        return opcao
+        retorno, values = self.open()
+        dicionario = {
+            'Animais' : 1,
+            'Pessoas' : 2,
+            'Adoçoes' : 3,
+            'Doações' : 4,
+            'Habitações' : 5,
+            'Vacinações' : 6
+        }
+        self.close()
+        return dicionario[retorno]
 
     def init_components(self):
-        sg.ChangeLookAndFeel('DarkGreen2')
+
+        titulo = ('Helvetica', 30)
+        botao_font = ('Helvetica', 20)
         layout = [
-            [sg.Text('Um texto')]
+            [sg.Text('Escolha seu menu',size=(20,1), font=titulo)],
+            [
+                sg.Button('Animais', size=20, font=botao_font),
+                sg.Button('Pessoas', size=20, font=botao_font),
+                sg.Button('Adoções', size=20, font=botao_font)
+            ],
+            [
+                sg.Button('Doações', size=20, font=botao_font),
+                sg.Button('Habitações', size=20, font=botao_font),
+                sg.Button('Vacinações', size=20, font=botao_font)
+            ]
         ]
-        self.__window = sg.Window('Sistema da ONG', default_element_size=(40,1)).Layout(layout)
+        self.__window = sg.Window('Sistema da ONG', default_element_size=(200,1)).Layout(layout)
 
     def open(self):
         button, values = self.__window.Read()
