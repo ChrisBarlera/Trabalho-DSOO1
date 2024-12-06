d0a00339bdb6312bddda611ba839994ab24b46b2
Christian Barlera <christian.barlera2@gmail.com>
2024-12-06 18:10:37 -0300

    Iniciando TelaAdotante

--- MVC/limite/TelaAdotante.py
+++ MVC/limite/TelaAdotante.py
@@ -1,33 +1,70 @@
+import PySimpleGUI as sg
 from datetime import date as Date
 
 
 class TelaAdotante:
+
+    def __init__(self) -> None:
+        self.__window = None
+
     def tela_opcoes(self):
-        print('\n-------- ADOTANTE ----------')
-        print('1 - Incluir Adotante')
-        print('2 - Alterar Adotante')
-        print('3 - Listar Adotantes')
-        print('4 - Excluir Adotante')
-        print('0 - Retornar')
+        titulo = ('Helvetica', 30)
+        botao_font = ('Helvetica', 20)
+        layout = [
+            [sg.Text('Adotante',size=(20,1), font=titulo)],
+            [
+                sg.Button('Incluir', size=20, font=botao_font),
+                sg.Button('Alterar', size=20, font=botao_font),
+                sg.Button('Listar', size=20, font=botao_font),
+                sg.Button('Excluir', size=20, font=botao_font)
+            ],
+            [sg.Button('Retornar', size=20, font=botao_font)]
+        ]
+        self.__window = sg.Window('Sistema da ONG', default_element_size=(200,1)).Layout(layout)
         
-        opcao = int(input('Escolha a opção: '))
-        return opcao
+        retorno, values = self.open()
+        dicionario = {
+            'Incluir' : 1,
+            'Alterar' : 2,
+            'Listar' : 3,
+            'Excluir' : 4,
+            'Retornar' : 0
+        }
+        self.close()
+        return dicionario[retorno]
+
     
     def pega_dados_adotante(self):
-        print('\n-------- DADOS ADOTANTE ----------')
-        cpf = int(input('CPF: '))
-        nome = input('Nome: ')
-        raw_data_nasc = input('Data de Nascimento (Formato dd/mm/yyyy): ').split('/')
-        data_dict = {'day': int(raw_data_nasc[0]),
+        titulo = ('Helvetica', 30)
+        botao_font = ('Helvetica', 20)
+        layout = [
+            [sg.Text('Insira os dados',size=(20,1), font=titulo)],
+            [sg.Text('CPF', size=20, font=botao_font),
+             sg.Input('Ex.: 12345678902', size=20, font=('Helvetica', 15), key='cpf')],
+            [sg.Text('Nome', size=20, font=botao_font),
+             sg.Input('Ex.: Fulaninho', size=20, font=('Helvetica', 15), key='nome')],
+            [sg.Text('Data de Nascimento (Formato dd/mm/yyyy)', size=20, font=botao_font),
+             sg.Input('Ex.: 13/12/2024', size=20, font=('Helvetica', 15), key='data_nasc')],
+            [sg.Text('Endereço completo', size=20, font=botao_font),
+             sg.Input('Ex.: Rua Fulano de tal', size=20, font=('Helvetica', 15), key='endereco')],
+            [sg.Checkbox(text='Já possui animal?',size=20, font=botao_font, key='possui_animais')],
+            [sg.Ok(size=20, font=botao_font)]
+        ]
+        self.__window = sg.Window('Sistema da ONG', default_element_size=(200,1)).Layout(layout)
+        button, values = self.open()
+        try:
+            values['cpf'] = int(values['cpf'])
+            raw_data_nasc = values['data_nasc'].split('/')
+            data_dict = {'day': int(raw_data_nasc[0]),
                      'month': int(raw_data_nasc[1]),
                      'year': int(raw_data_nasc[2])}
-        data_nasc = Date(**data_dict)
-        endereco = input('Seu endereço completo: ')
-        possui_animais = bool(int(input('Possui animais? (1: Sim, 0: Não): ')))
-
-        return {'cpf': cpf, 'nome': nome,
-                'data_nasc': data_nasc, 'endereco': endereco,
-                'possui_animais': possui_animais}
+            values['data_nasc'] = Date(**data_dict)
+        except:
+            self.mostra_mensagem('Preenchimento inválido dos dados!')
+            self.close()
+            self.pega_dados_adotante()
+        self.close()
+        return values
     
     def mostra_adotante(self, dados_adotante):
         print('\nCPF DO ADOTANTE: ', dados_adotante['cpf'])
@@ -36,22 +73,54 @@ class TelaAdotante:
         print('ENDEREÇO DO ADOTANTE: ', dados_adotante['endereco'])
         print('NÚEMERO DA HABITAÇÃO DO ADOTANTE: ', dados_adotante['habitacao'].numero)
         print('ADOTANTE POSSUI ANIMAIS?: ', dados_adotante['possui_animais'])
-
-    def mostra_adotante_especifico(self, dados_adotante):
-        print('NOME DO ADOTANTE:', dados_adotante['nome'])
-        print('CPF DO ADOTANTE:', dados_adotante['cpf'])
-
-    def seleciona_adotante(self):
-        cpf = int(input('\nCPF do adotante para selecionar: '))
-        return cpf
     
     def habitacao_ja_cadastrada(self):
-        resposta = bool(int(input('\nHabitação já cadastrada? (1: Sim, 0: Não): ')))
-        return resposta
+        titulo = ('Helvetica', 30)
+        botao_font = ('Helvetica', 20)
+        layout = [
+            [sg.Text('Habitação já cadastrada?',size=(20,1), font=titulo)],
+            [
+                sg.Button('Sim', size=20, font=botao_font),
+                sg.Button('Não', size=20, font=botao_font)
+            ],
+            [sg.Button('Retornar', size=20, font=botao_font)]
+        ]
+        self.__window = sg.Window('Sistema da ONG', default_element_size=(200,1)).Layout(layout)
+        
+        retorno, values = self.open()
+        dicionario = {
+            'Sim' : 1,
+            'Não' : 0
+        }
+        self.close()
+        return dicionario[retorno]
     
     def tbm_trocar_habitacao(self):
-        resposta = bool(int(input('\nTambém alterar habitação? (1: Sim, 0: Não): ')))
-        return resposta
+        titulo = ('Helvetica', 30)
+        botao_font = ('Helvetica', 20)
+        layout = [
+            [sg.Text('Também trocar habitação?',size=(20,1), font=titulo)],
+            [
+                sg.Button('Sim', size=20, font=botao_font),
+                sg.Button('Não', size=20, font=botao_font)
+            ]
+        ]
+        self.__window = sg.Window('Sistema da ONG', default_element_size=(200,1)).Layout(layout)
+        
+        retorno, values = self.open()
+        dicionario = {
+            'Sim' : 1,
+            'Não' : 0
+        }
+        self.close()
+        return dicionario[retorno]
     
-    def mostra_mensagem(self, msg):
-        print(f'\n{msg}')
+    def mostra_mensagem(self, mensagem: str, titulo='Mensagem'):
+        sg.Popup(titulo, mensagem)
+
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
+    
+    def close(self):
+        self.__window.Close()
