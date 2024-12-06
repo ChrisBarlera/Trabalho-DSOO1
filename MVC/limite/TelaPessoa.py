63bfec358c78a7664990afb38dc34bbee172d19b
Christian Barlera <christian.barlera2@gmail.com>
2024-12-06 14:27:20 -0300

    Início básico de TelaPessoa

--- MVC/limite/TelaPessoa.py
+++ MVC/limite/TelaPessoa.py
@@ -1,25 +1,56 @@
+import PySimpleGUI as sg
 from datetime import date as Date
 
 
 class TelaPessoa:
+    def __init__(self) -> None:
+        self.__window = None
+
     def tela_opcoes(self):
-        print('\n-------- PESSOA ----------')
-        print('1 - Incluir Pessoa')
-        print('2 - Alterar Pessoa')
-        print('3 - Listar Pessoas')
-        print('4 - Excluir Pessoa')
-        print('0 - Retornar')
+        titulo = ('Helvetica', 30)
+        botao_font = ('Helvetica', 20)
+        layout = [
+            [sg.Text('Pessoas',size=(20,1), font=titulo)],
+            [
+                sg.Button('Incluir', size=20, font=botao_font),
+                sg.Button('Alterar', size=20, font=botao_font),
+                sg.Button('Listar', size=20, font=botao_font),
+                sg.Button('Excluir', size=20, font=botao_font)
+            ],
+            [sg.Button('Retornar', size=20, font=botao_font)]
+        ]
+        self.__window = sg.Window('Sistema da ONG', default_element_size=(200,1)).Layout(layout)
         
-        opcao = int(input('Escolha a opção: '))
-        return opcao
+        retorno, values = self.open()
+        dicionario = {
+            'Incluir' : 1,
+            'Alterar' : 2,
+            'Listar' : 3,
+            'Excluir' : 4,
+            'Retornar' : 0
+        }
+        self.close()
+        return dicionario[retorno]
 
     def decide_tipo_pessoa(self):
-        print('\n-------- TIPO PESSOA ----------')
-        print('1 - Doador')
-        print('2 - Adotante')
-
-        opcao = int(input('Escolha a opção: '))
-        return opcao
+        titulo = ('Helvetica', 30)
+        botao_font = ('Helvetica', 20)
+        layout = [
+            [sg.Text('Escolha o tipo:',size=(20,1), font=titulo)],
+            [
+                sg.Button('Doador', size=20, font=botao_font),
+                sg.Button('Adotante', size=20, font=botao_font)
+            ]
+        ]
+        self.__window = sg.Window('Sistema da ONG', default_element_size=(200,1)).Layout(layout)
+        
+        retorno, values = self.open()
+        dicionario = {
+            'Doador' : 1,
+            'Adotante' : 2
+        }
+        self.close()
+        return dicionario[retorno]
 
     def decide_mostra_tipo(self):
         print('\n-------- TIPO PESSOA ----------')
@@ -30,3 +61,13 @@ class TelaPessoa:
 
         opcao = int(input('Escolha a opção: '))
         return opcao
+
+    def mostra_mensagem(self, mensagem: str, titulo='Mensagem'):
+        sg.Popup(titulo, mensagem)
+
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
+
+    def close(self):
+        self.__window.Close()
